2f7167e5a738b258ef754f7edc634f660a340adf
mariorocha2308 <virgoroch852@gmail.com>
2023-01-03 12:48:16 -0600

    update to continues next year

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ function App() {
   const currentDay = new Date().toLocaleString('en-us',{month:'short', day:'numeric'})
   const [year, setYear] = useLocalStorage("newYear", newYear.getFullYear());
   const onHandleFull = useFullScreenHandle();
+
   const onHandleWaitNextYear = () => {
     setTimeout(() => {
       setYear(newYear.getFullYear() + 1)
@@ -22,14 +23,14 @@ function App() {
     <Box position='relative'>
       <MdFullscreen className='full-screen-float-button' onClick={onHandleFull.enter}/>
       <FullScreen handle={onHandleFull} className='full-screen-toogle'>
-        {currentDay === 'Jan 1' || currentDay === 'Jan 2' ? 
+        {currentDay === 'Dec 31' || currentDay === 'Jan 1' ? 
           <Countdown
-            date={new Date(`Jan 2, 2023 00:00:00`)}
+            date={new Date(`Jan 1, ${year} 00:00:00`)}
             renderer={Renderer}
             onComplete={onHandleWaitNextYear}
           /> : 
           <Box display='flex' justifyContent='center' alignItems='center' fontFamily='Poppins' flexDirection='column'>
-            <Text fontSize='150' className="new-year" fontWeight='bold'>{newYear.getFullYear()}</Text>
+            <Text fontSize='150' className="new-year" fontWeight='bold'>{year - 1}</Text>
           </Box>
         }
       </FullScreen>
